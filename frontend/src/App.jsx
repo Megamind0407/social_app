62857954ab5e147c9eb1dd5f2686699ec8f80ca3
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2024-11-24 16:36:44 +0530

    Add -> features, testimonials, footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,17 @@
 import React from 'react'
 import { Navbar } from './components/Navbar'
 import { Hero } from './components/Hero'
+import Features from './components/Features'
+import { Testimonials } from './components/Testimonials'
+import Footer from './components/Footer'
 export const App = () => {
   return (
     <>
     <Navbar/>
     <Hero/>
+    <Features/>
+    <Testimonials/>
+    <Footer/>
     </>
   )
 }
