bf7aa5a611a766da5a20f050263cf5128e4a1a5b
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2024-11-24 22:43:04 +0530

    add routes->signin, signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-import { Navbar } from './components/Navbar'
-import { Hero } from './components/Hero'
+import  {Navbar} from './components/Navbar'
+import  {Hero}  from './components/Hero'
 import Features from './components/Features'
-import { Testimonials } from './components/Testimonials'
+import {Testimonials}  from './components/Testimonials'
 import Footer from './components/Footer'
-export const App = () => {
+import { Route, Routes } from 'react-router-dom'
+import { SignIn } from './components/pages/SignIn'
+import { SignUp } from './components/pages/SignUp'
+
+const HomePage = () => {
   return (
     <>
     <Navbar/>
@@ -13,6 +17,18 @@ export const App = () => {
     <Features/>
     <Testimonials/>
     <Footer/>
+    </>
+  )
+}
+
+export const App = () => {
+  return (
+    <>
+    <Routes>
+      <Route path='/signup' element= {<SignUp/>}/>
+      <Route path='/signin' element= {<SignIn/>}/>
+      <Route path='/' element= {<HomePage/>}/>
+    </Routes>
     </>
   )
 }
