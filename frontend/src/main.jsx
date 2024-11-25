7603a59ea7c90b5c6e85ee97c1c20045f91ac817
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2024-11-25 23:42:12 +0530

    add theme-provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
+import { ThemeProvider } from './components/ThemeContext';
 import './index.css';
 
 const rootElement = document.getElementById('root');
@@ -12,9 +13,11 @@ if (rootElement) {
 
   root.render(
     <StrictMode>
+      <ThemeProvider>
       <BrowserRouter>
           <App />
       </BrowserRouter>
+      </ThemeProvider>
     </StrictMode>
   );
 } else {
