0517b319a5dbd05bf5d850e8a848da9c1539bc04
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2024-11-25 23:43:41 +0530

    add theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, { useContext } from 'react';
 import  {Navbar} from './components/Navbar'
 import  {Hero}  from './components/Hero'
 import Features from './components/Features'
@@ -8,6 +8,8 @@ import Footer from './components/Footer'
 import { Route, Routes } from 'react-router-dom'
 import { SignIn } from './components/pages/SignIn'
 import { SignUp } from './components/pages/SignUp'
+import { ThemeContext } from './components/ThemeContext'; // Import ThemeContext
+
 
 const HomePage = () => {
   return (
@@ -22,14 +24,15 @@ const HomePage = () => {
 }
 
 export const App = () => {
+  const { theme } = useContext(ThemeContext); // Get the current theme
   return (
-    <>
+    <div className={`min-h-screen ${theme === 'dark' ? 'dark' : ''}`}>
     <Routes>
       <Route path='/signup' element= {<SignUp/>}/>
       <Route path='/signin' element= {<SignIn/>}/>
       <Route path='/' element= {<HomePage/>}/>
     </Routes>
-    </>
+    </div>
   )
 }
 
