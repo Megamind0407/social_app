92ca6e5e05fab3b07255b8676f75d07bc5bdb2e9
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2024-11-27 00:00:39 +0530

    update hero section

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -4,11 +4,11 @@ import { Button } from 'antd'
 
 export const Hero = () => {
     return (
-        <section className="h-full flex flex-col mt-10 justify-start text-primary text-center px-4">
+        <section className=" dark:bg-slate-900 h-full flex flex-col pt-10 dark:text-white justify-start text-primary text-center px-4">
             <h1 className="text-5xl md:text-6xl font-extrabold leading-tight mb-6">Connect with Passionate Hobbyists</h1>
             <p className="text-xl md:text-2xl mb-8">Discover, share, and learn new hobbies with a community that shares your interests!</p>
             <div className='mb-20'>
-                <Button className="font-semibold" type="primary" color="default" variant="filled" size='large'>Join Hobbyly</Button>
+                <Button className="font-semibold dark:bg-slate-50" color="default" size='large'>Join Hobbyly</Button>
             </div>
         </section>
     );
