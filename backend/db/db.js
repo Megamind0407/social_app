702c9e321d1141df3541e167add9273e0cfc835c
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2024-12-02 23:21:47 +0530

    init mongodb

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log('MongoDB created successfully')
+        console.log('MongoDB connected successfully')
     }catch(err){
         console.log(err.message);
         process.exit(1);
